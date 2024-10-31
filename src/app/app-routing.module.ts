f00999dc18c320cc6583b82eea658fe6d747700f
sgora96 <sergio_late@hotmail.com>
2024-10-31 00:11:52 -0500

    Se ajusta ruta del login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RegisterComponent } from './modules/register/register.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   // ... otras rutas
 ];
 
